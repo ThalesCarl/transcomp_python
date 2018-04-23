c02f9b5c9371533eb5dd2bdfcd036b5dacdddc45
Thales Carl Lavoratti <tclavoratti@sinmec.ufsc.br>
2018-04-23 18:01:35 -0300

    implementando matriz

--- transiente_implicito.py
+++ transiente_implicito.py
@@ -13,12 +13,13 @@ Created on Mon Apr 23 16:01:21 2018
 rho = 2700.0
 cp = 900.0
 k = 230
+h = 20000
 r0 = 0.05
 wallLength = 0.005
 numberOfNodes = 5;
 
 #############################
-# Geração da malha
+# Mesh generation
 #############################
 
 nodePositions = []
@@ -31,8 +32,21 @@ for i in range(numberOfNodes):
 accumulator = r0
 surfacePositions.append(r0)
 accumulator += 0.5*delta;
-for i in range(numberOfNodes):
+for i in range(numberOfNodes - 1):
     surfacePositions.append(accumulator)
     accumulator += delta
 accumulator -= 0.5*delta;
 surfacePositions.append(accumulator)
+
+###########################
+# Matrix coefficients
+###########################
+
+A=[]
+A.append([])
+A[0].append(1)
+for i in range(numberOfNodes - 1):
+    A[0].append(0)
+    
+for i in range(1,numberOfNodes - 1):
+    
