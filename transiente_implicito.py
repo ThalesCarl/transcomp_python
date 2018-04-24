abdfbaa59c6887143f73a0a547c4fd54a2db500b
Thales Carl Lavoratti <tclavoratti@sinmec.ufsc.br>
2018-04-24 10:06:58 -0300

    matrix implementada

--- transiente_implicito.py
+++ transiente_implicito.py
@@ -5,7 +5,8 @@ Created on Mon Apr 23 16:01:21 2018
 
 @author: tclavoratti
 """
-
+import math as mt
+pi = mt.pi
 ##############################
 #  Setting input paramethers
 ##############################
@@ -14,9 +15,14 @@ rho = 2700.0
 cp = 900.0
 k = 230
 h = 20000
+Tinf = 80
 r0 = 0.05
 wallLength = 0.005
-numberOfNodes = 5;
+length = 1
+numberOfNodes = 5
+prescribedTemperature = 200
+deltaT = 0.001
+
 
 #############################
 # Mesh generation
@@ -25,28 +31,63 @@ numberOfNodes = 5;
 nodePositions = []
 surfacePositions = []
 accumulator = r0
-delta = wallLength/(numberOfNodes - 1)
+deltaR = wallLength/(numberOfNodes - 1)
 for i in range(numberOfNodes):
     nodePositions.append(accumulator)
-    accumulator += delta
+    accumulator += deltaR
 accumulator = r0
 surfacePositions.append(r0)
-accumulator += 0.5*delta;
+accumulator += 0.5*deltaR;
 for i in range(numberOfNodes - 1):
     surfacePositions.append(accumulator)
-    accumulator += delta
-accumulator -= 0.5*delta;
+    accumulator += deltaR
+accumulator -= 0.5*deltaR;
 surfacePositions.append(accumulator)
 
-###########################
-# Matrix coefficients
-###########################
+################################
+# Setting beginTemperatureFields
+################################
+oldTemperatureField = []
+for i in range(numberOfNodes):
+    oldTemperatureField.append(80)
+temperatureField = []
+
+########################################
+# Matrix coefficients and linear Vector
+########################################
 
 A=[]
+b = []
 A.append([])
+
+#First Volume
 A[0].append(1)
 for i in range(numberOfNodes - 1):
     A[0].append(0)
-    
+b.append(prescribedTemperature)
+
+#Central Volumes
 for i in range(1,numberOfNodes - 1):
+    ap0 = rho*cp*2*pi*nodePositions[i]*length * deltaR/deltaT
+    aw = k*2*pi*surfacePositions[i] * length/deltaR
+    ae = k*2*pi*surfacePositions[i+1] * length/deltaR
+    ap = ap0 + aw + ae
+    A.append([])
+    for j in range(numberOfNodes):
+        A[i].append(0)
+    A[i][i-1] = -aw
+    A[i][i] = ap
+    A[i][i+1] = -ae
+    b.append(ap0 * oldTemperatureField[i])
     
+#Last Volume
+ap0 = rho*cp*2*pi*0.5*nodePositions[numberOfNodes-1]*length * deltaR/deltaT
+aw = 2*pi*k*surfacePositions[numberOfNodes-1]*length/deltaR
+hAx = 2*pi*nodePositions[numberOfNodes-1]*h
+ap = aw + ap0 + hAx
+A.append([])
+for j in range(numberOfNodes):
+    A[numberOfNodes-1].append(0)
+A[numberOfNodes-1][numberOfNodes-2] = -aw
+A[numberOfNodes-1][numberOfNodes-1] = ap
+b.append(ap0 * oldTemperatureField[numberOfNodes-1] + hAx*Tinf)    
