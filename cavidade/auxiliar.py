2f86bef81cf87941ba9f3c9f2932ae32c9dbfc59
ThalesCarl <tclavoratti@gmail.com>
2018-06-28 10:27:07 -0300

    matrix implementation begins

---
+++ cavidade/auxiliar.py
@@ -0,0 +1,53 @@
+"""
+Aluno: Thales Carl Lavoratti (151000656)
+Funções auxiliares usadas no código do programa da cavidade
+"""
+import numpy as np
+
+#def gen(xNodes,yNodes,L,V,deltaT,rho,mi,u0,uStar,v0,vStar,method):
+xNodes = 3
+yNodes = 3
+L=1.0
+mi = 0.001
+rho = 1.0
+uStar = [[0,2,-3,0],[0,6,-7,0],[0,10,11,0]]
+vStar = [[0,0,0],[9,-2,1],[-7,4,-5],[0,0,0]]
+
+uCounter = (xNodes+1)*yNodes
+vCounter = xNodes*(yNodes+1)
+pCounter = xNodes*yNodes
+deltaX = L/xNodes
+deltaY = L/yNodes
+"""
+A matriz A  foi dividida em nove quadrantes para facilitar o código
+todos os quadrantes são gerados separadamente e depois concatenados.
+O vetor b foi dividido em trÊs subvetores para facilitar o código e que
+em seguida foram concatenados
+"""
+A00 = np.zeros((uCounter,uCounter))
+A01 = np.zeros((uCounter,vCounter))
+A02 = np.zeros((uCounter,pCounter))
+A10 = np.zeros((vCounter,uCounter))
+A11 = np.zeros((vCounter,vCounter))
+A12 = np.zeros((vCounter,pCounter))
+A20 = np.zeros((pCounter,uCounter))
+A21 = np.zeros((pCounter,vCounter))
+A22 = np.zeros((pCounter,pCounter))
+
+#####################
+#primeito quadrante
+#####################
+#bottom
+for j in range(uCounter):
+    if j == 0 or j == xNodes:
+        ap = 1.0
+        A00[j,j] = ap
+    else:
+        De = mi*deltaY/deltaX
+        Dw = mi*deltaY/deltaX
+        Dn = mi*deltaX/deltaY
+        Ds = mi*deltaX/(0.5*deltaY)
+        Mw = rho*deltaY*0.5*(uStar[0][j-1]+uStar[0][j])
+        Me = rho*deltaY*0.5*(uStar[0][j]+uStar[0][j+1])
+        Ms = rho*deltaX*0.5*(vStar[0][j-1]+vStar[0][j])
+        Mn = rho*deltaX*0.5*(vStar[1][j-1]+vStar[1][j])
--- cavidade/cavidade.py
+++ cavidade/cavidade.py
@@ -6,6 +6,7 @@ Código do problema da cavidade
 import math as mt
 import numpy as np
 import matplotlib.pyplot as plt
+import auxiliar as aux
 
 ##############################
 #  Setting input paramethers
@@ -13,7 +14,6 @@ import matplotlib.pyplot as plt
 yNodes = 3
 xNodes = 3
 L = 1.0 
-w = 1.0 #[m]
 topWallVelocity = 1.0 #[m/s]
 mi = 0.001 #[Pa * s]
 rho = 1.0 #[kg/m^3]
@@ -33,24 +33,29 @@ uFields = []
 uFields.append(initial_U_Field)
 vFields = []
 vFields.append(initial_V_Field)
+uCounter = (xNodes+1)*yNodes
+vCounter = xNodes*(yNodes+1)
 
-####################
-##Appling the method
-####################
-#u0 = initial_U_Field
-#v0 = initial_V_Field
-#for i in range(numberOfTimeSteps):
-#    uStar = u0
-#    vStar = v0
-#    difference = 1.0
-#    counter = 0
-#    while(difference > tolerance and counter < maxCounter):
-#        A, b = aux.generator(xNodes,yNodes,L,w,topWallVelocity,deltaT,rho,mi,u0,uStar,v0,vStar)
-#        #solution u and v
-#        #bota a solução em algum tipo de matriz com indice i
-#        uFields.append(u)
-#        uStar = u
-#        vStar = v
-#        ++counter
-#    u0 = u
-#    v0 = v
+###################
+#Appling the method
+###################
+u0 = initial_U_Field
+v0 = initial_V_Field
+for i in range(numberOfTimeSteps):
+    uStar = u0
+    vStar = v0
+    difference = 1.0
+    counter = 0
+    while(difference > tolerance and counter < maxCounter):
+        A, b = aux.gen(xNodes,yNodes,L,topWallVelocity,deltaT,rho,mi,u0,uStar,v0,vStar,method)
+        #solution u and v
+        solution = np.linalg.solve(A,b)
+        u,v,P = np.split(solution,[uCounter+1,uCounter+vCounter+1])
+        uStar = u
+        vStar = v
+        difference = 0.001
+        ++counter
+    uFields.append(u)
+    vFields.append(v)        
+    u0 = u
+    v0 = v
